401ea708d6cb8a7664ca80b9cfffa5a226902dca
Vinh <34644363+tnvinh2711@users.noreply.github.com>
2025-07-30 00:02:57 -0700

    fix resolution

--- fast_barcode_scanner/android/src/main/kotlin/com/jhoogstraat/fast_barcode_scanner/PigeonExtensions.kt
+++ fast_barcode_scanner/android/src/main/kotlin/com/jhoogstraat/fast_barcode_scanner/PigeonExtensions.kt
@@ -15,7 +15,7 @@ val Resolution.width: Int
 val Resolution.height: Int
     get() = when (this) {
         Resolution.SD480 -> 480
-        Resolution.HD720 -> 960
+        Resolution.HD720 -> 720
         Resolution.HD1080 -> 1080
         Resolution.HD4K -> 2160
     }
